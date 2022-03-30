a20ad82faafd0f73290cdcacb320e61abe7e3659
Aleksandar-G <72451741+Aleksandar-G@users.noreply.github.com>
2022-03-30 14:46:35 +0200

    feat(user.js): user router setup

---
+++ routes/user.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const { User } = require("../models/");
+const router = express.Router()
+
+//create a new user
+router.post('/', (req, res) => {
+
+})
+
+
+//delete user
+router.delete('/', (req, res) => {
+
+})
+
+//change password
+router.post('password/change', (req, res) => {
+
+})
+
+
+module.exports = router
