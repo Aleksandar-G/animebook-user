46fc0af6a6bec3d00dced59ab1d887998b9f1b1d
Aleksandar-G <72451741+Aleksandar-G@users.noreply.github.com>
2022-03-31 14:21:49 +0200

    feat(user.js): user route

--- routes/user.js
+++ routes/user.js
@@ -1,21 +1,44 @@
 const express = require("express")
-const { User } = require("../models/");
 const router = express.Router()
+const { createUser, changePassword, deleteUser } = require("../controllers/userController")
 
 //create a new user
 router.post('/', (req, res) => {
+
+    const created = createUser(req.body.username, req.body.email, req.body.password)
+
+    if (created) {
+        res.status(201)
+        res.send("saved")
+    } else {
+        res.status(500)
+    }
 
 })
 
 
 //delete user
 router.delete('/', (req, res) => {
+    const deleted = deleteUser(req.body.username)
 
+    if (deleted) {
+        res.status(200)
+        res.send("deleted")
+    } else {
+        res.status(500)
+    }
 })
 
 //change password
-router.post('password/change', (req, res) => {
+router.put('password', (req, res) => {
+    const passwordChanged = changePassword(req.body.newPassword)
 
+    if (passwordChagned) {
+        res.status(200)
+        res.send("password changed")
+    } else {
+        res.status(500)
+    }
 })
 
 
