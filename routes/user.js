2d7fbac460970166ff6313f4f2efc2b8d6161bf7
Aleksandar-G <72451741+Aleksandar-G@users.noreply.github.com>
2022-04-06 15:18:03 +0200

    feat(user.js): userRoute udpate

--- routes/user.js
+++ routes/user.js
@@ -1,44 +1,79 @@
 const express = require("express")
 const router = express.Router()
-const { createUser, changePassword, deleteUser } = require("../controllers/userController")
+const { createUser, changePassword, deleteUser, checkUser } = require("../controllers/userController")
+const rabbitmq = require('../rabbitmq')
 
-//create a new user
-router.post('/', (req, res) => {
+//connect to rabbitmq
 
-    const created = createUser(req.body.username, req.body.email, req.body.password)
+// queues to connect to 
+const Verifyqueue = 'verify_auhtentication_queue'
+const Generatequeue = 'generate_auhtentication_queue'
 
-    if (created) {
-        res.status(201)
-        res.send("saved")
-    } else {
-        res.status(500)
-    }
+let channel;
 
+rabbitmq.rabbitMQChannel()
+    .then((ch) => {
+        channel = ch
+    })
+
+//login
+router.get('/', (req, res) => {
+
+    const username = req.body.username
+    const password = req.body.password
+
+    checkUser(username, password).then((userExists) => {
+        if (userExists) {
+            res.status(200)
+            res.send('ok')
+        } else {
+            res.status(401)
+            res.send("not authenticated")
+        }
+    })
 })
 
+//register
+router.post('/', (req, res) => {
+
+    createUser(req.body.username, req.body.email, req.body.password).then((created) => {
+
+        rabbitmq.sendRPCRequest(channel, req.body.username, Generatequeue)
+            .then((token) => {
+                if (created && token) {
+                    res.status(201)
+                    res.send({ token: token })
+                } else {
+                    res.status(500)
+                }
+            })
+    })
+})
 
 //delete user
 router.delete('/', (req, res) => {
-    const deleted = deleteUser(req.body.username)
-
-    if (deleted) {
-        res.status(200)
-        res.send("deleted")
-    } else {
-        res.status(500)
-    }
+    deleteUser(req.body.username).then((deleted) => {
+        if (deleted) {
+            res.status(200)
+            res.send("deleted")
+        } else {
+            res.status(500)
+        }
+    })
 })
 
 //change password
 router.put('password', (req, res) => {
-    const passwordChanged = changePassword(req.body.newPassword)
+    changePassword(req.body.newPassword).then(() => {
+        if (passwordChagned) {
+            res.status(200)
+            res.send("password changed")
+        } else {
+            res.status(500)
+        }
+    })
 
-    if (passwordChagned) {
-        res.status(200)
-        res.send("password changed")
-    } else {
-        res.status(500)
-    }
+
 })
 
 
