7dfd37efa483c6769955ebb47a84df058b4e265a
Aleksandar-G <72451741+Aleksandar-G@users.noreply.github.com>
2022-04-06 15:17:16 +0200

    feat(rabbitmq.js): rabbitmq controller utils

---
+++ rabbitmq.js
@@ -0,0 +1,66 @@
+require('dotenv').config()
+const amqp = require('amqplib');
+const EventEmitter = require('events');
+
+const messageBroker = process.env.MESSAGE_BROKER
+
+// queues to connect to 
+const Verifyqueue = 'verify_auhtentication_queue'
+const Generatequeue = 'generate_auhtentication_queue'
+
+
+const rabbitMQChannel = async () =>
+    amqp.connect(`amqp://localhost`)
+        .then((connection) => connection.createChannel())
+        .then((channel) => {
+
+            channel.assertQueue(Verifyqueue, {
+                durable: true
+            })
+
+            channel.assertQueue(Generatequeue, {
+                durable: true
+            })
+
+            channel.prefetch(1)
+
+            channel.responseEmitter = new EventEmitter();
+
+            return channel
+        })
+
+
+
+const sendRPCRequest = (channel, message, rpcQueue) => new Promise(resolve => {
+    // unique random string
+    const correlationId = generateUuid();
+
+    channel.assertQueue("", { exclusive: true, noAck: true })
+        .then((queue) => {
+            console.log("here");
+            channel.consume(queue.queue, msg =>
+                channel.responseEmitter.emit(msg.properties.correlationId, msg.content.toString()),
+                { noAck: true })
+
+            channel.responseEmitter.once(correlationId, resolve);
+
+            channel.sendToQueue(rpcQueue,
+                Buffer.from(message), {
+                correlationId: correlationId,
+                replyTo: queue.queue
+            });
+
+        })
+
+
+});
+
+const generateUuid = () => {
+    return Math.random().toString() +
+        Math.random().toString() +
+        Math.random().toString();
+}
+
+
+module.exports.rabbitMQChannel = rabbitMQChannel
+module.exports.sendRPCRequest = sendRPCRequest
