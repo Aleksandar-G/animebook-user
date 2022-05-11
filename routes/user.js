0e2214e761a19a49809e97bfc4e3fb9249140a43
Aleksandar-G <72451741+Aleksandar-G@users.noreply.github.com>
2022-05-11 16:45:56 +0200

    refactor(user.js): lint code and set cookies on login and register

--- routes/user.js
+++ routes/user.js
@@ -1,95 +1,94 @@
-const express = require("express")
-const router = express.Router()
-const { createUser, changePassword, deleteUser, checkUser } = require("../services/userService")
-const { login, register } = require('../controllers/userController')
-const rabbitmq = require('../rabbitmq')
+const express = require("express");
+const router = express.Router();
+const {
+  createUser,
+  changePassword,
+  deleteUser,
+  checkUser,
+} = require("../services/userService");
+const { login, register } = require("../controllers/userController");
+const rabbitmq = require("../rabbitmq");
 
 //connect to rabbitmq
 let channel;
 
-rabbitmq.rabbitMQChannel()
-    .then((ch) => {
-        channel = ch
-    })
+rabbitmq.rabbitMQChannel().then((ch) => {
+  channel = ch;
+});
 
 //login
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
-    const username = req.body.username
-    const password = req.body.password
-
-
-    login(channel, username, password).then((token) => {
-        console.log(token);
-        if (token === null) {
-            res.status(401)
-            res.send("not authenticated")
-        } else {
-            res.send({ token: token })
-        }
-    })
-
-})
+  login(channel, username, password).then((token) => {
+    if (token === null) {
+      res.status(401);
+      res.send("not authenticated");
+    } else {
+      res.cookie("token", token);
+      res.send({
+        username: username,
+        token: token,
+      });
+    }
+  });
+});
 
 //register
-router.post('/register', (req, res) => {
-
-    const username = req.body.username
-    const email = req.body.email
-    const password = req.body.password
-
-    register(channel, username, email, password).then((token) => {
-        if (token) {
-            res.status(201)
-            res.send({ token: token })
-        } else {
-            res.status(500)
-        }
-    })
-})
+router.post("/register", (req, res) => {
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
+  console.log(req);
+  register(channel, username, email, password).then((token) => {
+    if (token) {
+      res.cookie("token", token);
+      res.status(201);
+      res.send({
+        username: username,
+        token: token,
+      });
+    } else {
+      res.status(500);
+    }
+  });
+});
 
 //delete user
-router.delete('/delete', (req, res) => {
+router.delete("/delete", (req, res) => {
+  const rpcMessage = {
+    token: req.headers.authorization,
+    username: req.body.username,
+  };
 
-    const rpcMessage = { "token": req.headers.authorization, "username": req.body.username }
-
-    rabbitmq.sendRPCRequest(channel, rpcMessage, Verifyqueue)
-        .then((verified) => {
-
-            if (!verified) {
-                res.status(401)
-                res.send("not authenticated")
-
-            } else {
-                deleteUser(req.body.username).then((deleted) => {
-                    if (deleted) {
-                        res.status(200)
-                        res.send("deleted")
-                    } else {
-                        res.status(500)
-                    }
-                })
-            }
-
-
-        })
-
-
-})
+  rabbitmq.sendRPCRequest(channel, rpcMessage, Verifyqueue).then((verified) => {
+    if (!verified) {
+      res.status(401);
+      res.send("not authenticated");
+    } else {
+      deleteUser(req.body.username).then((deleted) => {
+        if (deleted) {
+          res.status(200);
+          res.send("deleted");
+        } else {
+          res.status(500);
+        }
+      });
+    }
+  });
+});
 
 //change password
-router.put('/password', (req, res) => {
-    changePassword(req.body.newPassword).then(() => {
-        if (passwordChagned) {
-            res.status(200)
-            res.send("password changed")
-        } else {
-            res.status(500)
-        }
-    })
+router.put("/password", (req, res) => {
+  changePassword(req.body.newPassword).then(() => {
+    if (passwordChagned) {
+      res.status(200);
+      res.send("password changed");
+    } else {
+      res.status(500);
+    }
+  });
+});
 
-
-})
-
-
-module.exports = router
+module.exports = router;
