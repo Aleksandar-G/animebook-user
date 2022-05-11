8fed8b179f0b1295ce31d6577c6cf59740897b8c
Aleksandar-G <72451741+Aleksandar-G@users.noreply.github.com>
2022-05-11 16:42:05 +0200

    feat(app.js): add cookiePaarser

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const { database } = require("./utils/database");
 const userRouter = require("./routes/user");
 
@@ -11,6 +12,9 @@ app.use(express.json());
 
 //enable cors
 app.use(cors());
+
+//cookies
+app.use(cookieParser());
 
 //routers
 app.use("/user", userRouter);
