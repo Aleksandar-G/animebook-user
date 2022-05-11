b0e67a00582bf691c0cb52d2855bf26db3db20c5
Aleksandar-G <72451741+Aleksandar-G@users.noreply.github.com>
2022-05-11 16:41:09 +0200

    refactor(userService.js): lint the code and return user insted of bool on creation of user

--- services/userService.js
+++ services/userService.js
@@ -1,65 +1,59 @@
+const bcrypt = require("bcrypt");
+const { User } = require("../models");
 
-const bcrypt = require("bcrypt")
-const { User } = require("../models")
-
-const saltRounds = 10
+const saltRounds = 10;
 
 const createUser = async (username, email, password) => {
+  const salt = await bcrypt.genSalt(saltRounds);
 
-    const salt = await bcrypt.genSalt(saltRounds)
-
-    return bcrypt.hash(password, salt).then((hash) => {
-
-        return User.create({ username: username, email: email, password: hash }).then(() => {
-            return true
-        }).catch((err) => {
-            console.error(err);
-            return false
-        })
-    })
-}
+  return bcrypt.hash(password, salt).then((hash) => {
+    return User.create({ username: username, email: email, password: hash })
+      .then((user) => {
+        return user;
+      })
+      .catch((err) => {
+        console.error(err);
+        return null;
+      });
+  });
+};
 
 const checkUser = async (username, password) => {
+  const user = await User.findOne({ where: { username: username } });
 
-    const user = await User.findOne({ where: { username: username } })
+  if (user === null) return null;
 
-    if (user === null) return false
-
-    return bcrypt.compare(password, user.password).then((comparePassword) => {
-
-        if (comparePassword) {
-            return true
-        } else {
-            return false
-        }
-    })
-
-
-}
-
+  return bcrypt.compare(password, user.password).then((comparePassword) => {
+    if (comparePassword) {
+      return user;
+    } else {
+      return null;
+    }
+  });
+};
 
 const changePassword = async (newPassword) => {
+  const hash = await bcrypt.hash(newPassword, saltRounds);
 
-    const hash = await bcrypt.hash(newPassword, saltRounds)
-
-    return User.update({ password: hash }).then(() => {
-        return true
-    }).catch((err) => {
-        console.log(err);
-        return false
+  return User.update({ password: hash })
+    .then(() => {
+      return true;
     })
-}
+    .catch((err) => {
+      console.log(err);
+      return false;
+    });
+};
 
 const deleteUser = async (username) => {
-    return User.destroy({
-        where: {
-            username: username
-        }
-    }
-    )
-}
+  return User.destroy({
+    where: {
+      username: username,
+    },
+  });
+};
 
-exports.createUser = createUser
-exports.changePassword = changePassword
-exports.deleteUser = deleteUser
-exports.checkUser = checkUser
+exports.createUser = createUser;
+exports.changePassword = changePassword;
+exports.deleteUser = deleteUser;
+exports.checkUser = checkUser;
